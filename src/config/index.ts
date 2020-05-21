b68e88dd597717c01ef59a2bffce2ef1aa794711
thanhtung <buithanhtung265@gmail.com>
2020-05-21 13:58:17 +0700

    update

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,5 +1,5 @@
-// export const DOMAIN_API = 'https://api.ditech.vn';
-export const DOMAIN_API = 'http://localhost';
+export const DOMAIN_API = 'https://api.ditech.vn';
+// export const DOMAIN_API = 'http://localhost';
 export const LINK_MENU = DOMAIN_API + '/backend/cm/menu/e3f56e40-94b0-4e1f-9830-7c7f0d1bd354/';
 export const LINK_HOME_PAGE = DOMAIN_API + '/backend/cm/page/';
 export const LINK_RIBBON_DETAIL = DOMAIN_API + '/backend/cm/ribbon/';
